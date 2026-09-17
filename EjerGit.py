3055154a985a5a0828f1b75008265cfdc9dd7b96
sofiasoler29 <solersofia29@gmail.com>
2026-09-17 17:20:10 -0500

    Operaciones matematicas

--- EjerGit.py
+++ EjerGit.py
@@ -1,4 +1,35 @@
-print("hello world")
-print("welcome")
-print("julian david arias rubio, arlet sofia jimenez soler")
-print("grado 10-6")
+print("OPERACIONES MATEMATICAS")
+print("Suma")
+a=10
+b=5
+resultado=a+b
+print("El resultado de la suma de 10 y 5 es")
+print(resultado)
+
+print("Resta")
+resultado=a-b
+print("El resultado de la resta de 10 y 5 es")
+print(resultado)
+
+print("Multiplicacion")
+resultado=a*b
+print("El resultado de la multiplicacion de 10 y 5 es")
+print(resultado)
+
+print("Division")
+b=4
+resultado=a/b
+print("La division exacta de 10 y 4 es")
+print(resultado)
+
+print("Division Entera")
+resultado=a//b
+print("El entero de la division de 10 y 4 es")
+print(resultado)
+
+print("Potenciacion")
+a=2
+b=3
+resultado=a**b
+print("2 elevado a la potencia de 3 es")
+print (resultado)
